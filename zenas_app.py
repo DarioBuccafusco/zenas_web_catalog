1233bc3aa7c279a3fa7b8125efe7b84c9ff55b90
Gruppo 1 <36633837+DarioBuccafusco@users.noreply.github.com>
2023-08-28 11:44:56 +0200

    Update zenas_app.py

--- zenas_app.py
+++ zenas_app.py
@@ -18,4 +18,6 @@ df = pandas.DataFrame(my_catalog)
 #streamlit.write(df)
 
 color_list = df[0].values.tolist()
-streamlit.write(color_list)
+#streamlit.write(color_list)
+
+option = streamlit.selectbox("Pick a sweatsuit color or style:' list(color_list))
