64cf285014fae5a32e9951f8ccb1efbb43a59295
Gruppo 1 <36633837+DarioBuccafusco@users.noreply.github.com>
2023-08-28 11:40:36 +0200

    Update zenas_app.py
    typo

--- zenas_app.py
+++ zenas_app.py
@@ -9,7 +9,7 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
 my_cur.execute("select color_or_style from catalog_for_website")
-my_catalog = my_cur.fetchall
+my_catalog = my_cur.fetchall()
 
 #put data into dataframe
 df = pandas.DataFrame(my_catalog)
