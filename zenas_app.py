32131fc915be778ce3f9c394d222852a4d01c81d
Gruppo 1 <36633837+DarioBuccafusco@users.noreply.github.com>
2023-08-28 11:34:03 +0200

    Update zenas_app.py
    writing color or style df

--- zenas_app.py
+++ zenas_app.py
@@ -1,11 +1,18 @@
 import streamlit
 import snowflake.connector
+import pandas
 
-streamlit.header("Healthy diner")
+streamlit.header("Zena's Amazing Catalog")
 
+#connector to snowflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+
+my_cur.execute("select color_or_style from catalog_for_website")
+my_catalog = my_cur.fetchall
+
+#put data into dataframe
+df = pandas.DataFrame(my_catalog)
+
+#write dataframe to the page
+streamlit.write(df)
